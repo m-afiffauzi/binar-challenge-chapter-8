7388e44c682c7c6a84311067c9c25ccda0f8a915
Muhammad Afif Fauzi <afiffauzi234@gmail.com>
2022-07-04 21:24:36 +0700

    add swagger-ui and client-ui

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const apiRouter = require('./server/routes')
-const errorHandler = require('./server/middlewares/errorHandler')
-const PORT = process.env.PORT || 4000
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const apiRouter = require("./server/routes");
+const errorHandler = require("./server/middlewares/errorHandler");
+const swaggerJSON = require("./openapi.json");
+const swaggerUI = require("swagger-ui-express");
+const PORT = process.env.PORT || 4000;
 
 // middlewares
-app.use(cors())
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
-app.use(errorHandler)
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(errorHandler);
 
 /**
  * @Routes /api
  * entrypoint for all API routes
  */
-app.use("/api", apiRouter)
+app.use("/api", apiRouter);
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerJSON));
 
 app.listen(PORT, () => {
-  console.log(`Listening on http://localhost:${PORT}`)
-})
+  console.log(`Listening on http://localhost:${PORT}`);
+});
